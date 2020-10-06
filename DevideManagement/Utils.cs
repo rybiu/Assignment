e577a03d2f19f51fe115f300daa7321e99378cbc
rybiu <ngocnguyen.cutelove@gmail.com>
2020-10-06 09:29:50 +0700

    Add project files.

---
+++ DevideManagement/Utils.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.SqlClient;
+
+namespace DevideManagement.Utils
+{
+    class DBHelper
+    {
+        const string DATA_SOURCE = "SE141150";
+        const string DATABASE_NAME = "management";
+
+        public static SqlConnection getConnection()
+        {
+
+            string connetionString = @"Data Source=" + DATA_SOURCE + ";Initial Catalog=" + DATABASE_NAME + ";Integrated Security=True";
+            SqlConnection con = new SqlConnection(connetionString);
+            return con;
+        }
+    }
+}
