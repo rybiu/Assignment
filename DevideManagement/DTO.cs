0b1d0c463a84c30f8fd2bffd15992d9af139b0a2
rybiu <ngocnguyen.cutelove@gmail.com>
2020-10-06 09:43:59 +0700

    1st commit

--- DevideManagement/DTO.cs
+++ DevideManagement/DTO.cs
@@ -69,4 +69,56 @@ namespace DevideManagement.DTO
         }
 
     }
+
+    public class RequestDTO
+    {
+        public enum STATUS { SENT, STARTED, FINISHED }
+        public int id { get; set; }
+        public string requestDescription { get; set; }
+        public DateTime requestDate { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+        public string takeoverDescription { get; set; }
+        public int userId { get; set; }
+        public int deviceId { get; set; }
+        public int workerId { get; set; }
+        public STATUS status { get; set; }
+
+        public RequestDTO(int id, int userId, int workerId, STATUS status)
+        {
+            this.id = id;
+            this.userId = userId;
+            this.workerId = workerId;
+            this.status = status;
+        }
+
+        public RequestDTO(int id, String requestDes, DateTime requestDate, DateTime startDate, DateTime endDate, String takeoverDes, int userId, int workerId, STATUS status)
+        {
+            this.id = id;
+            this.requestDescription = requestDes;
+            this.requestDate = requestDate;
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.takeoverDescription = takeoverDes;
+            this.userId = userId;
+            this.workerId = workerId;
+            this.status = status;
+        }
+    }
+
+    public class RoomDTO
+    {
+        public int id { get; set; }
+        public int name { get; set; }
+        public int numberUser { get; set; }
+        public int numberDevide { get; set; }
+
+        public RoomDTO(int id, int name, int numberUser, int numberDevide)
+        {
+            this.id = id;
+            this.name = name;
+            this.numberUser = numberUser;
+            this.numberDevide = numberDevide;
+        }
+    }
 }
